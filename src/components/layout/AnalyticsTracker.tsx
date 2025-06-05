2e1c3f375805f1e94ab4fcd54e288d1481f319aa
Abdulrahman Alshafei <alshafei.2430@gmail.com>
2025-06-05 10:34:31 +0300

    add missing deps in useEffect

--- src/components/layout/AnalyticsTracker.tsx
+++ src/components/layout/AnalyticsTracker.tsx
@@ -6,49 +6,48 @@ import { useEffect } from "react";
 import { useShallow } from "zustand/shallow";
 
 type AnalyticsTrackerProps = {
-    user: Partial<User> | null;
-}
-const AnalyticsTracker = ({user}: AnalyticsTrackerProps) => {
+  user: Partial<User> | null;
+};
+const AnalyticsTracker = ({ user }: AnalyticsTrackerProps) => {
+  const { cartId } = useCartStore(
+    useShallow((state) => ({
+      cartId: state.cartId,
+    }))
+  );
 
-    const { cartId } = useCartStore(
-        useShallow((state) => ({
-            cartId: state.cartId
-        }))
-    );
+  useEffect(() => {
+    if (!cartId || user) {
+      return;
+    }
 
-    useEffect(() => {
-        if(!cartId || user) {
-            return;
-        }
+    try {
+      const anyWindow = window as any;
 
-        try {
-            const anyWindow = window as any;
+      if (anyWindow.umami) {
+        anyWindow.umami.identify({
+          cartId,
+        });
+      }
+    } catch (e) {}
+  }, [cartId, user]);
 
-            if(anyWindow.umami) {
-                anyWindow.umami.identify({
-                    cartId,
-                })
-            }
-        } catch(e) {}
-    }, [cartId]);
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
 
-    useEffect(() => {
-        if(!user) {
-            return;
-        }
+    try {
+      const anyWindow = window as any;
 
-        try {
-            const anyWindow = window as any;
+      if (anyWindow.umami) {
+        anyWindow.umami.identify({
+          email: user.email,
+        });
+      }
+    } catch (e) {}
+  }, [user]);
 
-            if(anyWindow.umami) {
-                anyWindow.umami.identify({
-                    email: user.email,
-                })
-            }
-        } catch(e) {}
-    }, [user])
-
-    return <></>
-}
+  return <></>;
+};
 
 export default AnalyticsTracker;
