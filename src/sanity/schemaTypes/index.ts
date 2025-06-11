fc2e5b55e5b90b68bb87cf82b008b2883b77b88c
Abdulrahman Alshafei <alshafei.2430@gmail.com>
2025-06-11 11:37:42 +0300

    fix sanity errors and removed old package-lock.json

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,21 +1,20 @@
-import { order, orderItem, shippingAddress } from '@/sanity/schemaTypes/schemas/order'
-import { product } from '@/sanity/schemaTypes/schemas/product'
-import { productCategory } from '@/sanity/schemaTypes/schemas/product-category'
-import { promotionCampaign } from '@/sanity/schemaTypes/schemas/promotion-campaign'
-import { promotionCode } from '@/sanity/schemaTypes/schemas/promotion-codes'
-import { type SchemaTypeDefinition } from 'sanity'
+import { promotionCode } from "./schemas/promotion-codes";
+import { promotionCampaign } from "./schemas/promotion-campaign";
+import { productCategory } from "./schemas/product-category";
+import { product } from "./schemas/product";
+import { order, orderItem, shippingAddress } from "./schemas/order";
+import { type SchemaTypeDefinition } from "sanity";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     promotionCode,
     promotionCampaign,
 
-    
     productCategory,
     product,
-    
+
     shippingAddress,
     orderItem,
     order,
   ],
-}
+};
